486dcbcc49e0038e2e6769e421477108d059a757
isro <mohammadisro2710@gmail.com>
2025-11-27 21:45:19 +0700

    feat: add abis

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,20 +1,62 @@
-import { createConfig } from "ponder";
+import { createConfig } from "ponder"
+import { ADDRESSES } from "./src/constants/addresses"
 
-import { ExampleContractAbi } from "./abis/ExampleContractAbi";
+import { DustTokenAbi } from "./src/abis/DustTokenAbi"
+import { IdentityAbi } from "./src/abis/IdentityAbi"
+import { CoreAbi } from "./src/abis/CoreAbi"
+import { ContentAbi } from "./src/abis/ContentAbi"
+import { JobsAbi } from "./src/abis/JobsAbi"
+import { VerifierAbi } from "./src/abis/VerifierAbi"
 
 export default createConfig({
   chains: {
-    mainnet: {
-      id: 1,
-      rpc: process.env.PONDER_RPC_URL_1!,
+    lisk: {
+      id: 4202,
+      rpc: process.env.PONDER_RPC_URL_4202!,
     },
   },
+
   contracts: {
-    ExampleContract: {
-      chain: "mainnet",
-      abi: ExampleContractAbi,
-      address: "0x0000000000000000000000000000000000000000",
-      startBlock: 1234567,
+    DustToken: {
+      chain: "lisk",
+      address: ADDRESSES.DUST,
+      abi: DustTokenAbi,
+      startBlock: 34205941,
+    },
+
+    Identity: {
+      chain: "lisk",
+      address: ADDRESSES.IDENTITY,
+      abi: IdentityAbi,
+      startBlock: 34205941,
+    },
+
+    Core: {
+      chain: "lisk",
+      address: ADDRESSES.CORE,
+      abi: CoreAbi,
+      startBlock: 34205941,
+    },
+
+    Content: {
+      chain: "lisk",
+      address: ADDRESSES.CONTENT,
+      abi: ContentAbi,
+      startBlock: 34205941,
+    },
+
+    JobMarketplace: {
+      chain: "lisk",
+      address: ADDRESSES.JOBS,
+      abi: JobsAbi,
+      startBlock: 34205941,
+    },
+
+    TrustVerifier: {
+      chain: "lisk",
+      address: ADDRESSES.VERIFIER,
+      abi: VerifierAbi,
+      startBlock: 34205941,
     },
   },
-});
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
 import { ponder } from "ponder:registry";
+
+import "./handlers/dust/Transfer";
+import "./handlers/trustcore/TrustScoreUpdated";
+import "./handlers/trustcore/TierUpdated";
+import "./handlers/trustcore/RewardDistributed";
+import "./handlers/trustcore/PenaltyApplied";
+
+import "./handlers/postcontentnft/PostMinted";
+import "./handlers/postcontentnft/PostUpdated";
+import "./handlers/postcontentnft/PostBurned";
+
+import "./handlers/jobmarketplace/JobCreated";
+import "./handlers/jobmarketplace/JobApplied";
+import "./handlers/jobmarketplace/JobSelected";
+import "./handlers/jobmarketplace/JobSubmitted";
+import "./handlers/jobmarketplace/JobCompleted";
+
+import "./handlers/verifier/ProofSubmitted";
+import "./handlers/verifier/TierVerified";
